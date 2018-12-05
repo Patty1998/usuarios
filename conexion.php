ec692a4078b2010a24dc692b2edac6231fb0ecbf
LizSegura <45614910+LizSegura@users.noreply.github.com>
2018-12-04 22:56:38 -0500

    primeros cambios
    modifique la tabla y agregue campos

--- conexion.php
+++ conexion.php
@@ -1,3 +1,3 @@
 <?php
-	$conexion = new mysqli("localhost","root","","almacen1");
+	$conexion = new mysqli("localhost","root","","operaciones");
 ?>
--- tabla.php
+++ tabla.php
@@ -2,9 +2,57 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<title>Lista de Usuarios</title>
 	<link rel="stylesheet" type="text/css" href="css/tabla.css">
 </head>
 <body>
-	<center> BIENVENIDOS!! </center>
+	<center>
+		<table  border="3">
+			<thead>
+				<tr>
+					<th colspan="1"><a href="registrar.php">Nuevo</th>
+					<th colspan="7">Lista de Usuarios</th>	
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Id</td>
+					<td>Nombre</td>
+					<td>Apellido</td>
+					<td>Correo</td>
+					<td>Celular</td>
+					<td>Dni</td>
+					<td>Usuario</td>
+					<td>Password</td>
+					<td colspan="2">Operaciones</td>
+				</tr>
+				
+				<?php
+					include("conexion.php");
+					$query="SELECT * FROM usuarios";
+					$resultado =$conexion->query($query);
+					while ($row=$resultado->fetch_assoc()) {
+				?>
+
+					<tr>
+						<td><?php echo $row['id']; ?></td>
+						<td><?php echo $row['nombre']; ?></td>
+						<td><?php echo $row['apellido']; ?></td>
+						<td><?php echo $row['correo']; ?></td>
+						<td><?php echo $row['celular']; ?></td>
+						<td><?php echo $row['dni']; ?></td>
+						<td><?php echo $row['usuario']; ?></td>
+						<td><?php echo $row['password']; ?></td>
+						<td><a href="modificar.php?id=<?php echo $row['id']; ?>"> Modificar</a></td>
+						<td><a href="eliminar.php?id=<?php echo $row['id']; ?>"> Eliminar</a></td>
+					</tr>
+
+				<?php
+					}
+				?>
+
+			</tbody>
+		</table>
+	</center>
 </body>
 </html>
